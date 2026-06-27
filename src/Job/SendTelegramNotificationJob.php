f6055317446e9817f9565fe17831ee7d625b42b1
Sancho <77437810+sanchozu@users.noreply.github.com>
2026-06-27 11:39:00 +0300

    Modernize Telegram notification job

--- src/Job/SendTelegramNotificationJob.php
+++ src/Job/SendTelegramNotificationJob.php
@@ -1,13 +1,5 @@
 <?php
 
-
-/*
- * This file is part of Flarum.
- *
- * For detailed copyright and license information, please view the
- * LICENSE file that was distributed with this source code.
- */
-
 namespace Nodeloc\Telegram\Job;
 
 use Flarum\Notification\Blueprint\BlueprintInterface;
@@ -18,22 +10,15 @@ use Nodeloc\Telegram\Notifications\TelegramMailer;
 class SendTelegramNotificationJob extends AbstractJob
 {
     /**
-     * @var BlueprintInterface
+     * @param User[] $recipients
      */
-    private $blueprint;
-
-    /**
-     * @var User[]
-     */
-    private $recipients;
-
-    public function __construct(BlueprintInterface $blueprint,  array $recipients = [])
-    {
-        $this->blueprint = $blueprint;
-        $this->recipients = $recipients;
+    public function __construct(
+        protected BlueprintInterface $blueprint,
+        protected array $recipients = []
+    ) {
     }
 
-    public function handle(TelegramMailer $mailer)
+    public function handle(TelegramMailer $mailer): void
     {
         $mailer->send($this->blueprint, $this->recipients);
     }
