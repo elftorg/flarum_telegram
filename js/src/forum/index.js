d2af629a5669ff98d1334eecbced371f3443c812
Sancho <77437810+sanchozu@users.noreply.github.com>
2026-06-27 11:39:33 +0300

    Migrate forum frontend imports

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,11 +1,29 @@
-import app from 'flarum/app';
-import User from 'flarum/models/User';
-import Model from 'flarum/Model';
+import app from 'flarum/forum/app';
+import User from 'flarum/common/models/User';
+import Model from 'flarum/common/Model';
+
 import addLoginButton from './addLoginButton';
 import addNotificationMethod from './addNotificationMethod';
+
 app.initializers.add('nodeloc-telegram', () => {
     User.prototype.canReceiveTelegramNotifications = Model.attribute('canReceiveTelegramNotifications');
     User.prototype.nodelocTelegramError = Model.attribute('nodelocTelegramError');
+
+    showTelegramMessage();
     addLoginButton();
     addNotificationMethod();
 });
+
+function showTelegramMessage() {
+    const message = new URLSearchParams(window.location.search).get('telegramMessage');
+
+    if (!message || !app.alerts || typeof app.alerts.show !== 'function') {
+        return;
+    }
+
+    app.alerts.show({ type: 'success' }, message);
+
+    const url = new URL(window.location.href);
+    url.searchParams.delete('telegramMessage');
+    window.history.replaceState({}, document.title, url.toString());
+}
