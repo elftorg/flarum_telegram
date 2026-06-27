1dae7b327a3193db3fce95352423dc45963d6aad
Sancho <77437810+sanchozu@users.noreply.github.com>
2026-06-27 13:59:47 +0300

    Use Flarum 1.8 frontend import paths

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,11 +1,14 @@
-import app from 'flarum/app';
-import User from 'flarum/models/User';
-import Model from 'flarum/Model';
+import app from 'flarum/forum/app';
+import User from 'flarum/common/models/User';
+import Model from 'flarum/common/Model';
+
 import addLoginButton from './addLoginButton';
 import addNotificationMethod from './addNotificationMethod';
+
 app.initializers.add('nodeloc-telegram', () => {
     User.prototype.canReceiveTelegramNotifications = Model.attribute('canReceiveTelegramNotifications');
     User.prototype.nodelocTelegramError = Model.attribute('nodelocTelegramError');
+
     addLoginButton();
     addNotificationMethod();
 });
