9b739f0124bd2bbf3dcbbc23c984c73b64754941
Sancho <77437810+sanchozu@users.noreply.github.com>
2026-06-27 11:39:44 +0300

    Migrate Telegram login button frontend

--- js/src/forum/addLoginButton.js
+++ js/src/forum/addLoginButton.js
@@ -1,21 +1,28 @@
-import {extend} from 'flarum/extend';
-import LogInButtons from 'flarum/components/LogInButtons';
-import app from 'flarum/app';
+import { extend } from 'flarum/common/extend';
+import LogInButtons from 'flarum/forum/components/LogInButtons';
+import app from 'flarum/forum/app';
+import m from 'mithril';
 
 export default function () {
     extend(LogInButtons.prototype, 'items', function (items) {
-            const authUrl = app.forum.attribute('baseUrl') + '/auth/telegram';
-            const botUsername = app.forum.attribute('nodeloc-telegram.botUsername');
-            // Replace the TelegramProvide widget script
-            items.add('nodeloc-telegram',
-                m('script', {
-                    async: true, src: 'https://telegram.org/js/telegram-widget.js?22',
-                    'data-telegram-login': botUsername,
-                    'data-size': 'large',
-                    'data-radius': '10',
-                    'data-auth-url': authUrl,
-                    'data-request-access': 'write'
-                })
-            );
+        const botUsername = app.forum.attribute('nodeloc-telegram.botUsername');
+
+        if (!botUsername) {
+            return;
+        }
+
+        items.add('nodeloc-telegram', telegramLoginWidget('/'));
+    });
+}
+
+export function telegramLoginWidget(returnTo) {
+    return m('script', {
+        async: true,
+        src: 'https://telegram.org/js/telegram-widget.js?22',
+        'data-telegram-login': app.forum.attribute('nodeloc-telegram.botUsername'),
+        'data-size': 'large',
+        'data-radius': '10',
+        'data-auth-url': `${app.forum.attribute('baseUrl')}/auth/telegram?returnTo=${encodeURIComponent(returnTo)}`,
+        'data-request-access': 'write',
     });
 }
